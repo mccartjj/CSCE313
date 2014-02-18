80092c625451c52471e49f38af77e0daa7f39c52
mccartjj <mccartjj@email.sc.edu>
2014-02-18 00:08:46 -0500

    Beginning AA testing, Blue is fixed.

--- lights/software/lights/Project.c
+++ lights/software/lights/Project.c
@@ -16,12 +16,17 @@
 #include "altera_avalon_pio_regs.h"
 #include "alt_types.h"
 
+#define key3 3
+#define key2 5
+#define key1 6
+#define key0 7
+
 int main() {
 
-	//	alt_u32 current_value = 1;
-	//	alt_u32 current_state = 3;
-	//	alt_u8 current_direction = 0;
-	//	alt_u32 keys = 0;
+	alt_u32 current_value = 1;
+	alt_u32 current_state = 0;
+	alt_u8 current_direction = 0;
+	alt_u32 keys = 0;
 	setUpImage();
 
 	printf("Program running (UART)...\n");
@@ -29,12 +34,27 @@ int main() {
 
 	//the main program loop
 	while (1) {
-		printCharToScreen(0, 59, "Test 11: Apparently Silas is an old person.");
-		displayImage();
-		usleep(125000);
-		clearScreen();
-		constantRotation();
-		shrinkAndResize();
+		keys = IORD_ALTERA_AVALON_PIO_DATA(KEYS_BASE);
+
+		//switching speeds if necessary
+		if ((keys != key0) && (keys != current_state)) {
+			current_state = keys;
+		}
+
+		if (current_state == key3) {
+			printCharToScreen(0, 59, "Original Image");
+			displayImage();
+		}
+		else if (current_state == key2) {
+			constantRotation();
+		}
+		else if (current_state == key1){
+			shrinkAndResize();
+		}
+		else {
+			clearScreen();
+			printCharToScreen(5, 0, "John McCarthy and Silas Rubinson, Lab 2. Testing AA. Attempt 1.");
+		}
 	}
 	return 0;
 }
